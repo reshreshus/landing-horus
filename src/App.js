8c8150286ae153e6bb6dfa70b09467bd00cbe86a
reshreshus <reshreshus@gmail.com>
2019-12-24 20:18:55 +0300

    fix md-layout butchering css

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ export default class App extends Component {
       <div className="container">
         
         <div className="row justify-content-space-between">
-          <div className="col-12 col-md-6 mr-5">
+          <div className="col-12 col-md-6 mr-5 mt-5">
             <div className="horus-container">
               <img id="horus" src="horus_white.png" alt="Horus Eye"/>
               <Header />
             </div>
           </div>
-          <div className="col-12 col-md-5 ml-4">
+          <div className="col-12 col-sm-12 col-md-5 ml-4 small-medium-query">
             <InfoBlock />
           </div>
         </div>
