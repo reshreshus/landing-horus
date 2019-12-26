1a48399c4d57778390f45482d590c7eefab2219c
reshreshus <reshreshus@gmail.com>
2019-12-26 23:26:19 +0500

    links open in a new tab

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -5,22 +5,22 @@ export default function SocialLinks(props) {
     return (
         <div className={cName}>
              <div className="social-link-container">
-                <a href='https://t.me/reshreshus' className="nav-link">
+                <a href='https://t.me/reshreshus' className="nav-link" target="_blank">
                     <i className="fab fa-telegram fa-4x" />
                 </a> 
             </div>
             <div className="social-link-container">
-                <a href='mailto:merumeru.ceo@yandex.ru' className="nav-link">
+                <a href='mailto:merumeru.ceo@yandex.ru' className="nav-link" target="_blank">
                     <i className="far fa-envelope fa-4x" />
                 </a> 
             </div>
             <div className="social-link-container">
-                <a href='https://vk.com/reshreshus' className="nav-link">
+                <a href='https://vk.com/reshreshus' className="nav-link" target="_blank">
                     <i className="fab fa-vk fa-4x" />
                 </a> 
             </div>
             <div className="social-link-container">
-                <a href='#projects' className="nav-link">
+                <a href='#projects' className="nav-link" target="_blank">
                     <div className="d-flex align-items-center">
                         <i className="fas fa-project-diagram fa-4x" />
                         <h3>Projects</h3>
@@ -28,18 +28,18 @@ export default function SocialLinks(props) {
                 </a> 
             </div>
             <div className="social-link-container">
-                <a href='https://twitter.com/reshreshus2' className="nav-link">
+                <a href='https://twitter.com/reshreshus2' className="nav-link" target="_blank">
                 <i className="fab fa-twitter fa-4x" />
                 </a> 
             </div>
             <div className="social-link-container">
-                <a href='https://github.com/reshreshus' className="nav-link">
+                <a href='https://github.com/reshreshus' className="nav-link" target="_blank">
                     <i className="fab fa-github fa-4x" />
                 </a> 
             </div>
             
             <div className="social-link-container">
-                <a href='https://merume.ru' className="nav-link">
+                <a href='https://merume.ru' className="nav-link" target="_blank">
                     <h3>Blog</h3>
                     <i className="fas fa-bus fa-4x" />
                 </a> 
